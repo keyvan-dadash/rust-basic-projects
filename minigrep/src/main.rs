e715aeb771f8d8660ec95aa2f86140750adc92cf
sod-lol <ddssee5511@gmail.com>
2021-12-28 13:37:24 -0500

    main file changed for reading file content

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-
+use std::fs;
 
 fn main() {
 
@@ -8,7 +8,11 @@ fn main() {
     let query = &args[1];
     let filename = &args[2];
 
-
     println!("query {}", query);
     println!("filename {}", filename);
+
+    let content = fs::read_to_string(filename)
+                    .expect("Something went wrong when tried to read file");
+
+    println!("content is {}", content);
 }
