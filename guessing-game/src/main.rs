20de0411ed18192c3b55bd96ff3e05eb02cee9f0
sod-lol <ddssee5511@gmail.com>
2021-12-28 07:11:40 -0500

    rand crate added

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 
 fn main() {
     println!("Guessing Game");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
     println!("Please enter your guess.");
 
@@ -12,5 +16,18 @@ fn main() {
         .read_line(&mut guess)
         .expect("Cannot read line");
 
+
+    let guess: u32 = guess.trim().parse().expect("Type a number!");
+
     print!("You Guessed: {}", guess);
+
+    // for {
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => println!("You win"),
+        }
+    // }
+
+
 }
