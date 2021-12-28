ffb3c94ce2448682725b91c21bb385ff28545aa0
sod-lol <ddssee5511@gmail.com>
2021-12-28 07:23:46 -0500

    some files deleted

--- guessing-game/target/rls/debug/deps/guessing_game-cedb6be5dc4d9442.d
+++
@@ -1,5 +0,0 @@
-/home/sod/rust-learn/rust-projects-learning/guessing-game/target/rls/debug/deps/guessing_game-cedb6be5dc4d9442.rmeta: src/main.rs
-
-/home/sod/rust-learn/rust-projects-learning/guessing-game/target/rls/debug/deps/guessing_game-cedb6be5dc4d9442.d: src/main.rs
-
-src/main.rs:
