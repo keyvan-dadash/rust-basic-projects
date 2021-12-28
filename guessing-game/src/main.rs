7481b48cb5918d1d12f925ef7022e7806c53cb75
sod-lol <ddssee5511@gmail.com>
2021-12-28 04:29:16 -0500

    git ignore added, basic guessing game added

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game");
+
+    println!("Please enter your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Cannot read line");
+
+    print!("You Guessed: {}", guess);
 }
