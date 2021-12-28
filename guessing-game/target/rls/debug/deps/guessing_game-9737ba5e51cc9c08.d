0f281e2e298bf22f642118c0bb0f34170887ba3e
sod-lol <ddssee5511@gmail.com>
2021-12-28 04:14:14 -0500

    guessing game basic project structure added

---
+++ guessing-game/target/rls/debug/deps/guessing_game-9737ba5e51cc9c08.d
@@ -0,0 +1,5 @@
+/home/sod/rust-learn/rust-projects-learning/guessing-game/target/rls/debug/deps/guessing_game-9737ba5e51cc9c08.rmeta: src/main.rs
+
+/home/sod/rust-learn/rust-projects-learning/guessing-game/target/rls/debug/deps/guessing_game-9737ba5e51cc9c08.d: src/main.rs
+
+src/main.rs:
